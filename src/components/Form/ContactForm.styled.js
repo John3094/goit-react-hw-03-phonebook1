cd7bb8974f02eb986826005a0ccf1dbcd0993582
Yevhenii Yakovenko <yakovenkob@gmail.com>
2023-01-08 17:20:35 +0200

    finish

---
+++ src/components/Form/ContactForm.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  /* display: block; */
+  width: auto;
+  padding: 15px;
+  border: 1px solid #454545;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+`;
+
+export const AddButton = styled.button`
+  width: 150px;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
---
+++ src/components/List/Contact.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ContactItem = styled.li`
+  &:not(first-child) {
+    margin-top: 10px;
+  }
+`;
